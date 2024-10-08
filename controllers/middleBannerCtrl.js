d4c887b82c3e7c9b01776d05a235aa7037112e49
jsaraj <112096240+jsaraj@users.noreply.github.com>
2024-10-08 18:32:25 +0330

    Modify Delete, Update & New Method

--- controllers/middleBannerCtrl.js
+++ controllers/middleBannerCtrl.js
@@ -4,8 +4,8 @@ const middleBannerModel = require("../models/middleBannerModel");
 
 const getAllMidBanner = async (req, res) => {
     try {
-        if (req.query.pn) {
-            const paginate = 2;
+        if (req.query.pn && req.query.pgn) {
+            const paginate = req.query.pgn;
             const pageNumber = req.query.pn;
             const goalMidBanner = await middleBannerModel.find().sort({ _id: -1 }).skip((pageNumber - 1) * paginate).limit(paginate);
             const allGoalMidBan = await (await middleBannerModel.find()).length;
@@ -27,22 +27,25 @@ module.exports.getAllMidBanner = getAllMidBanner;
 
 const newMiddleBanner = async (req, res) => {
     try {
-        const newMiddleBanner = new midBannerModel({
-            imageUrl: req.body.imageUrl,
-            imageAlt: req.body.imageAlt,
-            imageLink: req.body.imageLink,
-            imageStatus: req.body.imageStatus,
-            publishDate: new Date().toLocaleDateString('fa-IR', { hour: '2-digit', minute: '2-digit' })
-        })
+        await middleBannerModel.create(req.body);
+        res.status(200).json({ msg: "بنر با موفقیت ذخیره شد" });
 
-        newMiddleBanner.save()
-            .then(d => {
-                res.status(200).json({ msg: "بنر با موفقیت ذخیره شد" });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(200).json({ msg: "خطا در دریافت اطلاعات" })
-            })
+        // const newMiddleBanner = new middleBannerModel({
+        //     imageUrl: req.body.imageUrl,
+        //     imageAlt: req.body.imageAlt,
+        //     imageLink: req.body.imageLink,
+        //     imageStatus: req.body.imageStatus,
+        //     publishDate: new Date().toLocaleDateString('fa-IR', { hour: '2-digit', minute: '2-digit' })
+        // })
+
+        // newMiddleBanner.save()
+        //     .then(d => {
+        //         res.status(200).json({ msg: "بنر با موفقیت ذخیره شد" });
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //         res.status(200).json({ msg: "خطا در دریافت اطلاعات" })
+        //     })
     }
     catch (err) {
         console.log(err);
@@ -56,11 +59,13 @@ module.exports.newMiddleBanner = newMiddleBanner;
 
 const deleteMiddleBanner = async (req, res) => {
     try {
-
-        await middleBannerModel.deleteOne(
-            { _id: req.body.goalId }
-        )
+        await middleBannerModel.findByIdAndRemove(req.params.id);
         res.status(200).json({ msg: "بنر با موفقیت حذف شد" });
+
+        // await middleBannerModel.deleteOne(
+        //     { _id: req.body.goalId }
+        // )
+
     }
     catch (err) {
         console.log(err);
@@ -73,7 +78,7 @@ module.exports.deleteMiddleBanner = deleteMiddleBanner;
 
 
 const getSingleMiddleBanner = async (req, res) => {
-    
+
     try {
         const goalSingleMidBan = await middleBannerModel.findById(req.params.id)
         res.status(200).json(goalSingleMidBan);
@@ -92,19 +97,22 @@ module.exports.getSingleMiddleBanner = getSingleMiddleBanner;
 const updateMiddleBanner = async (req, res) => {
     try {
 
-        await middleBannerModel.updateOne(
-            { _id: req.body.goalId },
-            {
-                $set: {
-                    imageUrl: req.body.imageUrl,
-                    imageAlt: req.body.imageAlt,
-                    imageLink: req.body.imageLink,
-                    imageStatus: req.body.imageStatus,
-                    publishDate: new Date().toLocaleDateString('fa-IR', { hour: '2-digit', minute: '2-digit' })
-                }
-            }
-        )
+        await middleBannerModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(200).json({ msg: "بنر با موفقیت بروزرسانی شد" });
+
+        // await middleBannerModel.updateOne(
+        //     { _id: req.body.goalId },
+        //     {
+        //         $set: {
+        //             imageUrl: req.body.imageUrl,
+        //             imageAlt: req.body.imageAlt,
+        //             imageLink: req.body.imageLink,
+        //             imageStatus: req.body.imageStatus,
+        //             publishDate: new Date().toLocaleDateString('fa-IR', { hour: '2-digit', minute: '2-digit' })
+        //         }
+        //     }
+        // )
+        // res.status(200).json({ msg: "بنر با موفقیت بروزرسانی شد" });
     }
     catch (err) {
         console.log(err);
@@ -113,3 +121,20 @@ const updateMiddleBanner = async (req, res) => {
 }
 
 module.exports.updateMiddleBanner = updateMiddleBanner;
+
+
+
+const getActiveMiddleBanner = async (req, res) => {
+
+    try {
+        const goalActiveMidBan = await middleBannerModel.find({ imageStatus: true }).select({ imageUrl: 1, imageAlt: 1, imageLink: 1 })
+        res.status(200).json(goalActiveMidBan);
+    }
+
+    catch (err) {
+        console.log(err);
+        res.status(400).json({ msg: "Error in get Active Middle Banner" })
+    }
+}
+
+module.exports.getActiveMiddleBanner = getActiveMiddleBanner;
--- routes/middleBannerRoutes.js
+++ routes/middleBannerRoutes.js
@@ -10,9 +10,10 @@ const middleBannerCtrl = require("../controllers/middleBannerCtrl");
 
 router.get("/middle-banner", middleBannerCtrl.getAllMidBanner);
 router.post("/new-middle-banner", middleBannerCtrl.newMiddleBanner);
-router.post("/update-middle-banner", middleBannerCtrl.updateMiddleBanner);
-router.post("/delete-middle-banner", middleBannerCtrl.deleteMiddleBanner);
+router.post("/update-middle-banner/:id", middleBannerCtrl.updateMiddleBanner);
+router.post("/delete-middle-banner/:id", middleBannerCtrl.deleteMiddleBanner);
 router.get("/get-single-middle-banner/:id", middleBannerCtrl.getSingleMiddleBanner);
+router.get("/get-active-middle-banner", middleBannerCtrl.getActiveMiddleBanner);
 
 
 module.exports = router;
